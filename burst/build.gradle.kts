306a998232ca6decfb0219b6aae7058681e0b5ea
Jake Wharton <jw@squareup.com>
2024-10-09 11:58:36 -0700

    Expand to all possible targets

--- burst/build.gradle.kts
+++ burst/build.gradle.kts
@@ -17,21 +17,39 @@ dependencies {
 }
 
 kotlin {
+  androidNativeArm32()
+  androidNativeArm64()
+  androidNativeX64()
+  androidNativeX86()
+
+  iosArm64()
+  iosSimulatorArm64()
+  iosX64()
+
+  js().nodejs()
+
   jvm()
 
-  js {
-    nodejs()
-  }
-
+  linuxArm64()
   linuxX64()
-  macosX64()
+
   macosArm64()
-  iosArm64()
-  iosX64()
-  iosSimulatorArm64()
+  macosX64()
+
+  mingwX64()
+
   tvosArm64()
   tvosSimulatorArm64()
   tvosX64()
+
+  wasmJs().nodejs()
+  wasmWasi().nodejs()
+
+  watchosArm32()
+  watchosArm64()
+  watchosDeviceArm64()
+  watchosSimulatorArm64()
+  watchosX64()
 
   applyDefaultHierarchyTemplate()
 
