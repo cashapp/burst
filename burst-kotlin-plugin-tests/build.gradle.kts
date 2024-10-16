cfa735c5fc45b993cadb82581e30a9d6ba5f8500
Jesse Wilson <jesse@swank.ca>
2024-10-16 12:35:46 -0400

    Target JDK8 (#25)
    * Target JDK8
    
    This library is consumed by Okio which is extremely strict
    on which JDKs it targets. We don't need anything newer anyway.
    
    * Add a toolchain repository
    
    ---------
    
    Co-authored-by: Jesse Wilson <jwilson@squareup.com>

--- burst-kotlin-plugin-tests/build.gradle.kts
+++ burst-kotlin-plugin-tests/build.gradle.kts
@@ -10,6 +10,4 @@ dependencies {
   testImplementation(kotlin("test-junit"))
   testImplementation(libs.assertk)
   testImplementation(libs.kotlin.compile.testing)
-  testImplementation(libs.kotlin.reflect)
-  testImplementation(libs.kotlinx.coroutines.test)
 }
