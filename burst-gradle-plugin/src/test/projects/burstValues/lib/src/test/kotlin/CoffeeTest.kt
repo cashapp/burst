170afe2b75b34741af52f83839c55b2d5e8ab056
Jesse Wilson <jwilson@squareup.com>
2024-10-28 17:52:01 -0400

    Gradle plugin test for burstValues

---
+++ burst-gradle-plugin/src/test/projects/burstValues/lib/src/test/kotlin/CoffeeTest.kt
@@ -0,0 +1,19 @@
+import app.cash.burst.Burst
+import app.cash.burst.burstValues
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+
+@Burst
+class CoffeeTest(
+  private val espresso: String = burstValues("Decaf", "Regular", "Double"),
+) {
+  @BeforeTest
+  fun setUp() {
+    println("set up $espresso")
+  }
+
+  @Test
+  fun test(size: Int = burstValues(8, 12, 16)) {
+    println("running $espresso $size")
+  }
+}
