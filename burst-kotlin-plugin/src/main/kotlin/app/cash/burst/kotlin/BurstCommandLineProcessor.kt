9bd045862da17f72fa08c6aa3177123b2e1a94c5
Jake Wharton <jw@squareup.com>
2025-10-22 13:20:20 -0400

    Remove KSP-based auto-service
    This was the only reason we were using KSP, and these values aren't changing.

--- burst-kotlin-plugin/src/main/kotlin/app/cash/burst/kotlin/BurstCommandLineProcessor.kt
+++ burst-kotlin-plugin/src/main/kotlin/app/cash/burst/kotlin/BurstCommandLineProcessor.kt
@@ -15,15 +15,13 @@
  */
 package app.cash.burst.kotlin
 
-import com.google.auto.service.AutoService
 import org.jetbrains.kotlin.compiler.plugin.CliOption
 import org.jetbrains.kotlin.compiler.plugin.CommandLineProcessor
 import org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi
 
 @OptIn(ExperimentalCompilerApi::class)
-@AutoService(CommandLineProcessor::class)
 class BurstCommandLineProcessor : CommandLineProcessor {
-  override val pluginId: String = BuildConfig.KOTLIN_PLUGIN_ID
+  override val pluginId: String get() = BuildConfig.KOTLIN_PLUGIN_ID
 
   override val pluginOptions: List<CliOption> = listOf()
 }
