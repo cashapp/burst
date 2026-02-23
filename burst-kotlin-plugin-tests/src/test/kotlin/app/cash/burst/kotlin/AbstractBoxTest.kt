07bd78333184389d4200f8f30c1652e2e669acdf
Derek Ellis <derekellis@squareup.com>
2026-02-23 13:35:38 -0500

    Add box tests (#298)

---
+++ burst-kotlin-plugin-tests/src/test/kotlin/app/cash/burst/kotlin/AbstractBoxTest.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2026 Cash App
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package app.cash.burst.kotlin
+
+import org.jetbrains.kotlin.config.JvmTarget
+import org.jetbrains.kotlin.test.builders.TestConfigurationBuilder
+import org.jetbrains.kotlin.test.directives.CodegenTestDirectives.IGNORE_DEXING
+import org.jetbrains.kotlin.test.directives.ConfigurationDirectives.WITH_STDLIB
+import org.jetbrains.kotlin.test.directives.JvmEnvironmentConfigurationDirectives.FULL_JDK
+import org.jetbrains.kotlin.test.directives.JvmEnvironmentConfigurationDirectives.JVM_TARGET
+import org.jetbrains.kotlin.test.runners.codegen.AbstractFirLightTreeBlackBoxCodegenTest
+import org.jetbrains.kotlin.test.services.KotlinStandardLibrariesPathProvider
+
+abstract class AbstractBoxTest : AbstractFirLightTreeBlackBoxCodegenTest() {
+  override fun createKotlinStandardLibrariesPathProvider(): KotlinStandardLibrariesPathProvider {
+    return ClasspathBasedStandardLibrariesPathProvider
+  }
+
+  override fun configure(builder: TestConfigurationBuilder) {
+    super.configure(builder)
+
+    with(builder) {
+      configurePlugin()
+
+      useSourcePreprocessor(::BoxTestImportPreprocessor)
+
+      defaultDirectives {
+        JVM_TARGET.with(JvmTarget.JVM_1_8)
+        +FULL_JDK
+        +WITH_STDLIB
+
+        +IGNORE_DEXING
+      }
+    }
+  }
+}
