0b7e10e114d527d8c5026ba21b66c4690edeb20a
Derek Ellis <derekellis@squareup.com>
2026-07-10 15:56:31 -0400

    Migrate remaining basic compiler plugin tests (#353)
    Delete the old compiler plugin tests now that we have full coverage in the compiler testing framework.

---
+++ burst-kotlin-plugin-tests/src/test/data/box/arguments/BooleanParameters.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2026 Cash App
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import app.cash.burst.Burst
+import kotlin.test.Test
+
+@Burst
+class CoffeeTest {
+  val log = mutableListOf<String>()
+
+  @Test
+  fun test(iced: Boolean) {
+    log += "running iced=$iced"
+  }
+}
+
+fun box(): String {
+  val instance = CoffeeTest()
+
+  instance.invokeSpecialization("test_false")
+  assertThat(instance.log).containsExactly("running iced=false")
+  instance.log.clear()
+
+  instance.invokeSpecialization("test_true")
+  assertThat(instance.log).containsExactly("running iced=true")
+  instance.log.clear()
+
+  return "OK"
+}
---
+++ burst-kotlin-plugin-tests/src/test/data/box/arguments/NullableEnumAsDefault.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2026 Cash App
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import app.cash.burst.Burst
+import kotlin.test.Test
+
+@Burst
+class CoffeeTest {
+  val log = mutableListOf<String>()
+
+  @Test
+  fun test(espresso: Espresso? = null) {
+    log += "running $espresso"
+  }
+}
+
+enum class Espresso {
+  Decaf,
+  Regular,
+  Double,
+}
+
+fun box(): String {
+  val instance = CoffeeTest()
+
+  instance.invokeSpecialization("test_Decaf")
+  instance.invokeSpecialization("test_Regular")
+  instance.invokeSpecialization("test_Double")
+  instance.invokeSpecialization("test")
+  assertThat(instance.log)
+    .containsExactly(
+      "running Decaf",
+      "running Regular",
+      "running Double",
+      "running null",
+    )
+
+  return "OK"
+}
